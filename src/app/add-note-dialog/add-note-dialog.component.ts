f4dc524a097f8725e9dd541ea9e3ebcf566ce3b1
Judith L. <judith.lenz@outlook.de>
2025-03-11 16:15:12 +0100

    delete note

--- src/app/add-note-dialog/add-note-dialog.component.ts
+++ src/app/add-note-dialog/add-note-dialog.component.ts
@@ -1,22 +1,22 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { Note } from '../interfaces/note.interface';
-import { NoteListService } from '../firebase-services/note-list.service'
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, Output, EventEmitter } from "@angular/core";
+import { Note } from "../interfaces/note.interface";
+import { NoteListService } from "../firebase-services/note-list.service";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-note-dialog',
+  selector: "app-add-note-dialog",
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './add-note-dialog.component.html',
-  styleUrl: './add-note-dialog.component.scss'
+  templateUrl: "./add-note-dialog.component.html",
+  styleUrl: "./add-note-dialog.component.scss",
 })
 export class AddNoteDialogComponent {
   @Output() addDialogClosed: EventEmitter<boolean> = new EventEmitter();
   title = "";
   description = "";
 
-  constructor(public noteService: NoteListService){}
+  constructor(public noteService: NoteListService) {}
 
   closeDialog() {
     this.title = "";
@@ -24,8 +24,14 @@ export class AddNoteDialogComponent {
     this.addDialogClosed.emit(false);
   }
 
-  addNote(){
-    //beachte das closeDialog() zum Schluss kommt, denn es leert die Variablen
+  addNote() {
+    let note: Note = {
+      type: "note",
+      title: this.title,
+      content: this.description,
+      marked: false,
+    };
+    this.noteService.addNote(note);
     this.closeDialog();
   }
 }
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,15 @@
 import { Injectable, inject } from "@angular/core";
 import { Note } from "../interfaces/note.interface";
-import { collectionData, Firestore, collection, doc, onSnapshot, addDoc } from "@angular/fire/firestore";
+import {
+  collectionData,
+  Firestore,
+  collection,
+  doc,
+  onSnapshot,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+} from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: "root",
@@ -19,7 +28,39 @@ export class NoteListService {
     this.unsubTrash = this.subTrashList();
   }
 
-  async addNote(item: {}) {
+  async deleteNote(colId: string, docId: string) {
+    await deleteDoc(this.getSingleDocRef(colId, docId)).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  async updateNote(note: Note) {
+    if (note.id) {
+      let docRef = this.getSingleDocRef(this.getColIdFromNote(note), note.id);
+      await updateDoc(docRef, this.getCleanJson(note)).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+
+  getCleanJson(note: Note): {} {
+    return {
+      type: note.type,
+      title: note.title,
+      content: note.content,
+      marked: note.marked,
+    };
+  }
+
+  getColIdFromNote(note: Note) {
+    if (note.type == "note") {
+      return "notes";
+    } else {
+      return "trash";
+    }
+  }
+
+  async addNote(item: Note) {
     await addDoc(this.getNotesRef(), item)
       .catch((err) => {
         console.error(err);
